f359220a576e09b56d7fdf09ae6681b1cdb4d3a9
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-09-18 16:49:56 +0200

    create repo

---
+++ src/main/java/kassandrafalsitta/u2w3d3/U2w3d3Application.java
@@ -0,0 +1,13 @@
+package kassandrafalsitta.u2w3d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2w3d3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2w3d3Application.class, args);
+	}
+
+}
---
+++ src/test/java/kassandrafalsitta/u2w3d3/U2w3d3ApplicationTests.java
@@ -0,0 +1,13 @@
+package kassandrafalsitta.u2w3d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2w3d3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
